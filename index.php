b324ac334c80be12d27a9c9c88282dfdc8e32998
akrez <akrez.like@gmail.com>
2026-01-19 01:15:38 +0330

    up

--- index.php
+++ index.php
@@ -8,8 +8,48 @@ $dg = new DigiGold(0, 170_000_000_0, 7);
 // die(json_encode($dg->getLastAnalyze()));
 ?>
 
-<?php
 
-foreach ($dg->getLastAnalyze() as $ayar => $ayarContent) {
-    var_export(array_slice($ayarContent,0,20));
-}
+<!doctype html>
+<html class="h-100" lang="fa">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"><!-- CSRF Token -->
+    <link rel="shortcut icon" href="">
+</head>
+
+<body dir="rtl">
+    <div style="display: flex; align-items: flex-start;">
+
+        <?php foreach ($dg->getLastAnalyze() as $ayar => $ayarRows) { ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th colspan="999">
+                            <?= $ayar ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach (array_slice($ayarRows, 0, 20) as $row) { ?>
+                        <tr>
+                            <td>
+                                <img src="<?= $row['image'] ?>" style="max-width: 128px; max-height: 128px;">
+                            </td>
+                            <td>
+                                <a target="_blank" href="<?= $row['url'] ?>"><?= $row['title_fa'] ?></a>
+                                <div><?= $row['seller'] ?></div>
+                                <div><?= $row['size'] ?></div>
+                                <div><?= $row['_selling_price_formatted'] ?></div>
+                                <div><?= $row['_price_per_gram_formatted'] ?></div>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php } ?>
+
+    </div>
+</body>
+
+</html>
