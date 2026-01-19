758f3c9cbf052cc53cf5fafcaff3e5d106f01583
akrez <akrez.like@gmail.com>
2026-01-20 02:33:50 +0330

    up

--- index.php
+++ index.php
@@ -17,6 +17,12 @@ $dg = new DigiGold();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./favicon.ico">
     <link href="./bootstrap.rtl.min.css" rel="stylesheet">
+    <style>
+        .max-50px {
+            max-width: 50px;
+            max-height: 50px;
+        }
+    </style>
 </head>
 
 <body dir="rtl">
@@ -29,23 +35,36 @@ $dg = new DigiGold();
                 <?php foreach ($dg->getLastAnalyze()['variants_ayar'] as $ayar => $variants) { ?>
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="alert alert-info my-3 text-center h4">عیار <?= $ayar ?></div>
+                            <div class="col-12">
+                                <div class="alert alert-info my-3 text-center h4">عیار <?= $ayar ?></div>
+                            </div>
                         </div>
                         <?php foreach (array_slice($variants, 0, 20) as $variant) { ?>
-                            <div class="row small border p-1">
-                                <div class="col-1">
-                                    <img src="<?= $variant['image'] ?>" class="img-fluid">
-                                </div>
-                                <div class="col-11">
-                                    <div>
-                                        <a class="text-decoration-none" target="_blank" href="<?= $variant['url'] ?>">
-                                            <?= $variant['title_fa'] ?>
-                                        </a>
-                                        <span><?= $variant['seller_title'] ?></span>
-                                    </div>
-                                    <div><strong>وزن</strong> <?= $variant['size'] ?></div>
-                                    <div><strong>قیمت</strong> <?= $variant['_selling_price_formatted'] ?></div>
-                                    <div><strong>قیمت هر گرم</strong> <?= $variant['_price_per_gram_formatted'] ?></div>
+                            <div class="row small">
+                                <div class="col-12">
+                                    <table class="table  table-bordered table-sm">
+                                        <tbody>
+                                            <tr>
+                                                <td rowspan="2">
+                                                    <img src="<?= $variant['image'] ?>" class="max-50px">
+                                                </td>
+                                                <td colspan="7">
+                                                    <a class="text-decoration-none" target="_blank" href="<?= $variant['url'] ?>">
+                                                        <?= $variant['title_fa'] ?>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><?= $variant['seller_title'] ?></td>
+                                                <td class="table-secondary fw-bold">وزن</td>
+                                                <td><?= $variant['size'] ?></td>
+                                                <td class="table-secondary fw-bold">قیمت هر گرم</td>
+                                                <td><?= $variant['_price_per_gram_formatted'] ?></td>
+                                                <td class="table-secondary fw-bold">قیمت</td>
+                                                <td><?= $variant['_selling_price_formatted'] ?></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         <?php } ?>
