31e5d2e16d5a73744628fb85bed45657d9594cc2
akrez <akrez.like@gmail.com>
2026-01-19 21:25:27 +0330

    up

--- DigiGold.php
+++ DigiGold.php
@@ -24,38 +24,42 @@ class DigiGold
         return $path;
     }
 
-    function sendMultiGet(array $urls, $fn)
+    function sendMultiGet(array $urls, $fn, $chunkLength = 20)
     {
-        $multiCurl = curl_multi_init();
+        foreach(array_chunk($urls, $chunkLength) as $chunkedUrls) {
 
-        foreach ($urls as $url) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                "accept: application/json, text/plain, */*",
-                "accept-language: en-US,en;q=0.9,fa;q=0.8",
-                "priority: u=1, i",
-                "sec-fetch-dest: empty",
-                "sec-fetch-mode: cors",
-                "sec-fetch-site: same-site",
-                "x-web-client: desktop",
-                "x-web-client-id: web",
-                "x-web-optimize-response: 1"
-            ]);
-            curl_multi_add_handle($multiCurl, $ch);
-        }
+            $multiCurl = curl_multi_init();
 
-        do {
-            curl_multi_exec($multiCurl, $running);
-        } while ($running > 0);
+            foreach ($chunkedUrls as $url) {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                    "accept: application/json, text/plain, */*",
+                    "accept-language: en-US,en;q=0.9,fa;q=0.8",
+                    "priority: u=1, i",
+                    "sec-fetch-dest: empty",
+                    "sec-fetch-mode: cors",
+                    "sec-fetch-site: same-site",
+                    "x-web-client: desktop",
+                    "x-web-client-id: web",
+                    "x-web-optimize-response: 1"
+                ]);
+                curl_multi_add_handle($multiCurl, $ch);
+            }
 
-        while (($info = curl_multi_info_read($multiCurl)) !== false) {
-            $ch = $info['handle'];
-            $response = curl_multi_getcontent($ch);
-            $fn($response);
-            curl_multi_remove_handle($multiCurl, $ch);
+            do {
+                curl_multi_exec($multiCurl, $running);
+            } while ($running > 0);
+
+            while (($info = curl_multi_info_read($multiCurl)) !== false) {
+                $ch = $info['handle'];
+                $response = curl_multi_getcontent($ch);
+                $fn($response);
+                curl_multi_remove_handle($multiCurl, $ch);
+            }
+    
         }
     }
 
@@ -69,34 +73,39 @@ class DigiGold
         file_put_contents($path, json_encode((array)$arrayContent, JSON_UNESCAPED_UNICODE));
     }
 
+    function searchList($fromPage, $toPage)
+    {
+        $urls = [];
+        for ($i = $fromPage; $i <= $toPage; $i++) {
+            $path = $this->path('search', $i . '.json');
+            if (! file_exists($path)) {
+                $urls[] = 'https://api.digikala.com/v1/categories/bullion/search/?has_selling_stock=1&page='.$i.'&sort=7';
+            }
+        }
+        if (empty($urls)) {
+            return;
+        }
+        $this->sendMultiGet($urls, function ($response) {
+            $response = json_decode($response, true);
+            if (! empty($response['data']['pager']['current_page'])) {
+                $path = $this->path('search', $response['data']['pager']['current_page'] . '.json');
+                $this->writeJson($path, $response);
+            }
+        });
+    }
+
     function search()
     {
         $page = 1;
-        while (true) {
-            $path = $this->path('search', $page . '.json');
-            if (! file_exists($path)) {
-                $urls = [];
-                for ($i = $page; $i < $page + 10; $i++) {
-                    $urls[] = 'https://api.digikala.com/v1/categories/bullion/search/?has_selling_stock=1&page='.$i.'&sort=7';
-                }
-
-                $this->sendMultiGet($urls, function ($response) {
-                    $response = json_decode($response, true);
-                    if (! empty($response['data']['pager']['current_page'])) {
-                        $path = $this->path('search', $response['data']['pager']['current_page'] . '.json');
-                        $this->writeJson($path, $response);
-                    }
-                });
-            }
-            if (
-                file_exists($path) &&
-                ($response = $this->readJson($path)) &&
-                (! empty($response['data']['products']))
-            ) {
-                $page++;
-            } else {
-                return $page;
-            }
+        $this->searchList(1, 1);
+        $path = $this->path('search', $page . '.json');
+        if (
+            file_exists($path) &&
+            ($response = $this->readJson($path)) &&
+            (! empty($response['data']['pager']['total_pages'])) && 
+            ($response['data']['pager']['total_pages'] > $page)
+        ) {
+            $this->searchList($page+1, $response['data']['pager']['total_pages']);
         }
     }
 
@@ -196,10 +205,11 @@ class DigiGold
                 //
                 '24 عیار' => 24,
                 '۲۴ عیار' => 24,
+                '24 عیـار' => 24,
                 //
                 '750 عیار' => 750,
                 '995 عیار' => 995,
-                '999.9 عیار' => 999,
+                '999.9 عیار' => 999.9,
             ] as $ayarKey => $ayarValue
         ) {
             if (stripos($productName, $ayarKey) !== false) {
