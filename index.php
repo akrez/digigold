3ef3957ce8df048bd2c7c31c78415a330387e9f0
akrez <akrez.like@gmail.com>
2026-01-20 03:25:50 +0330

    up

--- index.php
+++ index.php
@@ -16,11 +16,20 @@ $dg = new DigiGold();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./favicon.ico">
-    <link href="./bootstrap.rtl.min.css" rel="stylesheet">
+    <link href="./assets/bootstrap/css/bootstrap.rtl.min.css" rel="stylesheet">
+    <link href="./assets/sahel/css/sahel.css" rel="stylesheet">
     <style>
+        * {
+            font-family: Sahel;
+        }
+
         .max-50px {
             max-width: 50px;
             max-height: 50px;
+        }
+
+        .border-top-width-half {
+            border-top-width: 0.15rem !important;
         }
     </style>
 </head>
@@ -31,21 +40,21 @@ $dg = new DigiGold();
         <div class="row">
             <div class="col-sm-2">
             </div>
-            <div class="col-sm-8">
+            <div class="col-sm-8 mt-3">
                 <?php foreach ($dg->getLastAnalyze()['variants_ayar'] as $ayar => $variants) { ?>
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-12">
-                                <div class="alert alert-info my-3 text-center h4">عیار <?= $ayar ?></div>
-                            </div>
-                        </div>
-                        <?php foreach (array_slice($variants, 0, 20) as $variant) { ?>
-                            <div class="row small">
-                                <div class="col-12">
-                                    <table class="table  table-bordered table-sm">
-                                        <tbody>
-                                            <tr>
-                                                <td rowspan="2">
+                                <table class="table table-bordered table-sm">
+                                    <thead>
+                                        <tr class="border-top-width-half">
+                                            <th colspan="8" class="text-center table-info">عیار <?= $ayar ?></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach (array_slice($variants, 0, 20) as $variant) { ?>
+                                            <tr class="border-top-width-half">
+                                                <td rowspan="2" class="text-center align-middle">
                                                     <img src="<?= $variant['image'] ?>" class="max-50px">
                                                 </td>
                                                 <td colspan="7">
@@ -57,17 +66,17 @@ $dg = new DigiGold();
                                             <tr>
                                                 <td><?= $variant['seller_title'] ?></td>
                                                 <td class="table-secondary fw-bold">وزن</td>
-                                                <td><?= $variant['size'] ?></td>
+                                                <td class="font-monospace"><?= $variant['size'] ?></td>
                                                 <td class="table-secondary fw-bold">قیمت هر گرم</td>
-                                                <td><?= $variant['_price_per_gram_formatted'] ?></td>
+                                                <td class="font-monospace"><?= $variant['_price_per_gram_formatted'] ?></td>
                                                 <td class="table-secondary fw-bold">قیمت</td>
-                                                <td><?= $variant['_selling_price_formatted'] ?></td>
+                                                <td class="font-monospace"><?= $variant['_selling_price_formatted'] ?></td>
                                             </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
                             </div>
-                        <?php } ?>
+                        </div>
                     </div>
                 <?php } ?>
             </div>
