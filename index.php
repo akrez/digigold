c5507e2cb12b7088dd9cf2aae3ab69215437bf68
akrez <akrez.like@gmail.com>
2026-01-20 02:17:29 +0330

    up

--- index.php
+++ index.php
@@ -27,29 +27,29 @@ $dg = new DigiGold();
             </div>
             <div class="col-sm-8">
                 <?php foreach ($dg->getLastAnalyze()['variants_ayar'] as $ayar => $variants) { ?>
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="alert alert-info my-3 text-center h4">عیار <?= $ayar ?></div>
-                    </div>
-                    <?php foreach (array_slice($variants, 0, 20) as $variant) { ?>
-                    <div class="row small border p-1">
-                        <div class="col-1">
-                            <img src="<?= $variant['image'] ?>" class="img-fluid">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="alert alert-info my-3 text-center h4">عیار <?= $ayar ?></div>
                         </div>
-                        <div class="col-11">
-                            <div>
-                                <a class="text-decoration-none" target="_blank" href="<?= $variant['url'] ?>">
-                                    <?= $variant['title_fa'] ?>
-                                </a>
-                                <span><?= $variant['seller_title'] ?></span>
+                        <?php foreach (array_slice($variants, 0, 20) as $variant) { ?>
+                            <div class="row small border p-1">
+                                <div class="col-1">
+                                    <img src="<?= $variant['image'] ?>" class="img-fluid">
+                                </div>
+                                <div class="col-11">
+                                    <div>
+                                        <a class="text-decoration-none" target="_blank" href="<?= $variant['url'] ?>">
+                                            <?= $variant['title_fa'] ?>
+                                        </a>
+                                        <span><?= $variant['seller_title'] ?></span>
+                                    </div>
+                                    <div><strong>وزن</strong> <?= $variant['size'] ?></div>
+                                    <div><strong>قیمت</strong> <?= $variant['_selling_price_formatted'] ?></div>
+                                    <div><strong>قیمت هر گرم</strong> <?= $variant['_price_per_gram_formatted'] ?></div>
+                                </div>
                             </div>
-                            <div><strong>وزن</strong> <?= $variant['size'] ?></div>
-                            <div><strong>قیمت</strong> <?= $variant['_selling_price_formatted'] ?></div>
-                            <div><strong>قیمت هر گرم</strong> <?= $variant['_price_per_gram_formatted'] ?></div>
-                        </div>
+                        <?php } ?>
                     </div>
-                    <?php } ?>
-                </div>
                 <?php } ?>
             </div>
             <div class="col-sm-2">
