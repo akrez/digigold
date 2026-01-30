46a262796ae7b2510ff4ee38f8c037d1fa24a0f7
akrez <akrez.like@gmail.com>
2026-01-30 16:21:00 +0330

    up

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Tehran');
 set_time_limit(120);
 
 include 'DigiGold.php';
-$dg = new DigiGold(implode(DIRECTORY_SEPARATOR, [__DIR__, 'cache', date('Y-m-d-H-i')]));
+$dg = new DigiGold(implode(DIRECTORY_SEPARATOR, [__DIR__, 'cache', date('Y-m-d-H').'-'.intval(date('i')/5)*5]));
 $analyze = $dg->analyze();
 
 $analyzeFilePath = __DIR__ . DIRECTORY_SEPARATOR . 'analyze.json';
