86d7807da0a228881634b8580f6c174e5bd105b6
akrez <akrez.like@gmail.com>
2026-02-13 15:02:08 +0330

    up

--- public/update.php
+++ public/update.php
@@ -1,14 +1,22 @@
 <?php
 
+use App\DigiKalaGold;
+use App\Gold;
+use App\SnappShopGold;
+
 date_default_timezone_set('Asia/Tehran');
 set_time_limit(180);
 
-$currentPath = __DIR__ . DIRECTORY_SEPARATOR;
-$basePath = $currentPath . '..' . DIRECTORY_SEPARATOR;
+include implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'vendor', 'autoload.php']);
 
-include  $basePath . 'DigiGold.php';
-$dg = new DigiGold($basePath . implode(DIRECTORY_SEPARATOR, ['cache', date('Y-m-d-H').'-'.intval(date('i')/5)*5]));
-$analyze = $dg->analyze();
+$cacheKey = date('Y-m-d-H').'-'.intval((date('i') / 5) + 1) * 5;
 
-$analyzeFilePath = $currentPath . 'index.json';
-file_put_contents($analyzeFilePath, json_encode((array)$analyze, JSON_UNESCAPED_UNICODE));
+$gold = new Gold;
+$analyze = $gold->analyzeAll($cacheKey, [
+    'DigiKala' => DigiKalaGold::class,
+    'SnappShop' => SnappShopGold::class,
+]);
+file_put_contents(
+    implode(DIRECTORY_SEPARATOR, [__DIR__, 'index.json']),
+    json_encode((array) $analyze, JSON_UNESCAPED_UNICODE)
+);
